c1d65dbb61fa308786243fda054ca43a4440164f
Mykola Matsko <mykola.matsko@gmail.com>
2020-04-10 11:08:33 +0300

    first commit

---
+++ LBTA_UIKitFacebook2019/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LBTA_UIKitFacebook2019
+//
+//  Created by Mykola Matsko on 10.04.2020.
+//  Copyright © 2020 Mykola Matsko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
